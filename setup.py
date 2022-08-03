52108845eace01558004295fd9c0dd885abfaf8e
lucas <lucas.ma8338@gmail.com>
2022-08-03 16:09:54 -0300

    changed the setup 'requires' to 'install_requires'

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(name='lib_getdata',
-      version='5.0.0',
+      version='5.0.1',
       description='general purpose library to manipulate data',
       author='Lucas Monteiro',
       author_email='lucas.ma8338@gmail.com',
       url='https://github.com/lucas8338/lib-getdata',
       packages=find_packages(),
-      requires=['pandas', 'numpy', 'MetaTrader5', 'sklearn', 'scipy', 'tqdm', 'statsmodels', 'pyod', 'requests_futures', 'holidays',
+      install_requires=['pandas', 'numpy', 'MetaTrader5', 'sklearn', 'scipy', 'tqdm', 'statsmodels', 'pyod', 'requests_futures', 'holidays',
                 'pytrends'],
       )
+
+setup()
